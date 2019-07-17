9b06db09c1ab46b32e6a98ca26d2893f59258ca4
fanboyalliance <alex.raswqa@gmail.com>
2019-07-17 04:20:16 +0300

    update code

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Proxy
 {
@@ -14,11 +10,53 @@ namespace Proxy
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args);
+            var config = builder.Build();
+            StaticKeeper.Host = "localhost";
+            StaticKeeper.Port = "8080";
+            StaticKeeper.MainUri = "habr.com";
+            
+            if (config["host"] != null)
+            {
+                StaticKeeper.Host = config["host"];
+            }
+            if (config["port"] != null)
+            {
+                StaticKeeper.Port = config["port"];
+            }
+            if (config["site"] != null)
+            {
+                StaticKeeper.MainUri = config["site"];
+            }
+            
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseUrls($"http://{StaticKeeper.Host}:{StaticKeeper.Port}/")
+                .UseStartup<Startup>()
+                .Build();
+            OpenBrowser($"http://{StaticKeeper.Host}:{StaticKeeper.Port}/");
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            host.Run();
+        }
+        
+        private static void OpenBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+        }
     }
 }
